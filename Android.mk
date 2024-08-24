3de8767ff820ada4de7c3b58c78d7ad642281536
almond <42428221+almondnguyen@users.noreply.github.com>
2024-08-24 22:44:19 +0700

    Android.mk: remove conflict lib
    Signed-off-by: almond <42428221+almondnguyen@users.noreply.github.com>

--- Android.mk
+++ Android.mk
@@ -3,15 +3,15 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),grandppltedx)
 ifneq ($(RECOVERY_VARIANT),twrp)
 # libaudiopolicy 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libaudiopolicymanager
-LOCAL_MODULE_OWNER := MediaTek
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES_32 := proprietary/lib/libaudiopolicymanager.so
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libaudiopolicymanager
+#LOCAL_MODULE_OWNER := MediaTek
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_SUFFIX := .so
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MULTILIB := 32
+#LOCAL_SRC_FILES_32 := proprietary/lib/libaudiopolicymanager.so
+#include $(BUILD_PREBUILT)
 
 # dpframework
 include $(CLEAR_VARS)
